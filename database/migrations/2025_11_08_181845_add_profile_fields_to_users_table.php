ac36cbed5b3d38925c445d3f3271214a98ed1204
David Martínez Aliaga <158084789+DavidMartinezo039@users.noreply.github.com>
2025-11-15 20:23:32 +0100

    Hacer que username pueda ser nullable

--- database/migrations/2025_11_08_181845_add_profile_fields_to_users_table.php
+++ database/migrations/2025_11_08_181845_add_profile_fields_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('username')->unique()->after('name');
+            $table->string('username')->unique()->after('name')->nullable();
             $table->string('avatar')->nullable()->after('email');
             $table->text('bio')->nullable()->after('avatar');
         });
