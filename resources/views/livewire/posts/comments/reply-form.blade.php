1c368d194d6556a33a6a6ed1b9a4fdc14c0c0df7
MartaIniesta <158085005+MartaIniesta@users.noreply.github.com>
2026-01-29 13:29:41 +0100

    Notificaciones mejorado

--- resources/views/livewire/posts/comments/reply-form.blade.php
+++ resources/views/livewire/posts/comments/reply-form.blade.php
@@ -1,5 +1,18 @@
-<div class="create-post-box relative" x-data="{ dragging: false }" x-on:dragover.prevent="dragging = true"
-    x-on:dragleave.prevent="dragging = false"
+<article class="create-post-box relative" wire:key="reply-form-{{ $comment->id }}" x-data="{
+    dragging: false,
+    showLinkInput: false,
+    link: '',
+    addLink() {
+        if (!this.link) return;
+
+        this.$refs.replyTextarea.value += ' ' + this.link;
+        this.link = '';
+        this.showLinkInput = false;
+
+        this.$refs.replyTextarea.dispatchEvent(new Event('input'));
+    }
+}"
+    x-on:dragover.prevent="dragging = true" x-on:dragleave.prevent="dragging = false"
     x-on:drop.prevent="
         dragging = false;
         $wire.uploadMultiple('newReplyMedia', [...$event.dataTransfer.files])
@@ -8,7 +21,8 @@
     <div class="create-post-top">
         <img src="{{ asset(Auth::user()->avatar) }}" class="create-avatar">
 
-        <textarea wire:model.defer="replyContent" class="create-textarea" placeholder="Escribe una respuesta…"></textarea>
+        <textarea x-ref="replyTextarea" wire:model.defer="replyContent" class="create-textarea"
+            placeholder="Escribe una respuesta…"></textarea>
     </div>
 
     @include('livewire.posts.media', [
@@ -17,6 +31,14 @@
         'removeMethod' => 'removeReplyMedia',
     ])
 
+    <div x-show="showLinkInput" x-transition class="link-input">
+        <input type="url" class="link-input-field" placeholder="Pega un enlace (https://...)" x-model="link">
+
+        <button type="button" class="link-input-btn" @click="addLink">
+            Añadir enlace
+        </button>
+    </div>
+
     <div class="create-actions">
         <div class="left-actions">
             <label for="replyMediaInput-{{ $comment->id }}">
@@ -24,10 +46,14 @@
             </label>
 
             <input type="file" id="replyMediaInput-{{ $comment->id }}" wire:model="newReplyMedia" multiple hidden>
+
+            <button type="button" @click="showLinkInput = !showLinkInput" title="Añadir enlace">
+                <i class="bx bx-paperclip"></i>
+            </button>
         </div>
 
         <button wire:click="addReply" class="btn-post">
             Responder
         </button>
     </div>
-</div>
+</article>
